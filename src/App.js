2267780c8dae8e1e1c699828836428f9eea7d49c
arvindsingh-cedcommerce <104122345+arvindsingh-cedcommerce@users.noreply.github.com>
2022-07-20 10:57:14 +0530

    Image Gallery using props in Class Components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import PropstocallDisplayImage from './PropstocallDisplayImage';
+
+function App() {
+  return (
+    <div>
+      <PropstocallDisplayImage />
+    </div>
+  );
+}
+
+export default App;
